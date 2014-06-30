85b546931a30f09bb3423f6b9918c48d76aadb8f
Administrator <john@Joffrey.local>
2014-06-30 11:44:03 -0400

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Administrator on 6/30/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Administrator on 6/30/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
